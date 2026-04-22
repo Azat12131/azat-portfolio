1a102e7f8497af9f67a200801711771a84df34f4
Azat12131 <nurumburum2003@mail.ru>
2026-04-23 00:40:15 +0500

    fix 12

--- src/components/sections/projects/projectData.ts
+++ src/components/sections/projects/projectData.ts
@@ -229,7 +229,7 @@ export const caseArticles: Record<number, CaseArticle> = {
           '/projects/print/print-01.png',
           '/projects/print/print-02.png',
           '/projects/print/print-03.png',
-          '/projects/print/print-044.png',
+          '/projects/print/print-044.jpg',
           '/projects/print/print-05.png',
           '/projects/print/print-06.png',
           '/projects/print/print-07.png',
